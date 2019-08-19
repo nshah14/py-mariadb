5ef78a4e65c9a1d1ccc019a631b99de1d2f29dbe
Naved Shah <naved1482@gmail.com>
2019-08-19 14:27:18 +0100

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:2
+
+ADD insert.py /
+
+RUN pip install mysql-connector
+
+ENTRYPOINT ["python"]
+CMD [ "insert.py", "-i", "naved", "o" , "shah"]
---
+++ insert.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import sys, getopt
+import mysql.connector as mariadb
+import os
+
+ENV_USER=os.environ['ENV_USER']
+ENV_PASS=os.environ['ENV_PASS']
+ENV_DB=os.environ['ENV_DB']
+
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
+   insert_data(inputfile, outputfile)
+
+def insert_data( first_name, last_name):
+   print ' employee first_name', first_name
+   print 'employee last_name', last_name
+   mariadb_connection = connect_maria_db()
+   cursor = mariadb_connection.cursor()
+   cursor.execute("INSERT INTO employees (first_name, last_name) VALUES (%s, %s) ", (first_name, last_name))
+   mariadb_connection.commit()
+
+
+def connect_maria_db():
+     mariadb_connection = mariadb.connect(user=ENV_USER, password=ENV_PASS, database=ENV_DB)
+     return mariadb_connection
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
